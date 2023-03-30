1a86b5208eec9fd53f250dec8c3ba3a0eb3169e0
Simon Andersen <simo414u@stud.kea.dk>
2023-03-30 13:44:58 +0200

    All done

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,18 +1,42 @@
 <template>
     <div class="results">
-        Your reaction time was: {{ result }}
+        Your reaction time was: {{ result }} <br>
+        <h3>{{ rank }}</h3>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Results',
-    props: ['result']
+    props: ['result'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.result < 350) {
+            this.rank = 'Speedy Gonzales jajaja!'
+        } else if (this.result < 450) {
+            this.rank = "You're pretty, pretty, prettiii fast...!"
+        } else if (this.result < 650) {
+            this.rank = "Could be faster..."
+        } else {
+            this.rank = "Jebus you're slow dude!"
+        }
+        console.log(this.rank)
+    }
 }
+
 </script>
 
 <style scoped>
 .results {
     font-size: 3rem;
+}
+
+h3 {
+    font-weight: bolder;
+    color: brown;
 }
 </style>
